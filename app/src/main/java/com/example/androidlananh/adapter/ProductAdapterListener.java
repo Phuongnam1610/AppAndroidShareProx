e5c951b96dc45a3c8a392c5d941456f619550f1c
Phuong Nam <Phuongnam0316@gmail.com>
2025-04-20 13:24:27 +0700

    bỏ theo dõi các file build

--- app/src/main/java/com/example/androidlananh/adapter/ProductAdapterListener.java
+++ app/src/main/java/com/example/androidlananh/adapter/ProductAdapterListener.java
@@ -1,12 +1,12 @@
 package com.example.androidlananh.adapter;
 
 import com.example.androidlananh.model.Product;
-import com.example.androidlananh.ui.base.BaseView;
+import com.example.androidlananh.model.Product;
 
 import java.util.ArrayList;
 
-public interface ProductAdapterView extends BaseView {
+public interface ProductListener {
     void onClickProduct(Product product);
     void displayAllProduct(ArrayList<Product> products);
-    
+
 }
